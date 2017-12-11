73496232cdddaf23e98d4597c32e40f3376e293e
DzhafarovEmil <johndoe@example.com>
2017-12-11 21:48:50 +0200

    vfdvdj

--- src/main/java/emil/dzhafarov/dineit/controller/OrderController.java
+++ src/main/java/emil/dzhafarov/dineit/controller/OrderController.java
@@ -51,7 +51,7 @@ public class OrderController {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @RequestMapping(value = "/create-order/", method = RequestMethod.GET)
+    @RequestMapping(value = "/create-order/", method = RequestMethod.POST)
     public ResponseEntity<Long> createOrder(@RequestParam("food_company_id") Long foodCompanyId,
                                             @RequestBody Order order, Principal principal) {
         FoodCompany foodCompany = foodCompanyService.findById(foodCompanyId);
