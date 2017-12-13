209a05d86b72e0f9feaffef71da9396d11d39be6
DzhafarovEmil <johndoe@example.com>
2017-12-13 17:58:58 +0200

    gfydgvdyg

--- src/main/java/emil/dzhafarov/dineit/model/OrderStatus.java
+++ src/main/java/emil/dzhafarov/dineit/model/OrderStatus.java
@@ -1,16 +1,5 @@
 package emil.dzhafarov.dineit.model;
 
 public enum OrderStatus {
-    RECEIVED("Received"), IN_PROGRESS("In progress"), DONE("Done"), CREATED("Created");
-
-    private String string;
-
-    OrderStatus(String string) {
-        this.string = string;
-    }
-
-    @Override
-    public String toString() {
-        return string;
-    }
+    RECEIVED, IN_PROGRESS, DONE, CREATED;
 }
