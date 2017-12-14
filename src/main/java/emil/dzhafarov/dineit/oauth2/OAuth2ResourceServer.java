612fb2bb0fa3285beb460aa03170c5037ce62555
DzhafarovEmil <johndoe@example.com>
2017-12-14 20:13:30 +0200

    fvfd

--- src/main/java/emil/dzhafarov/dineit/oauth2/OAuth2ResourceServer.java
+++ src/main/java/emil/dzhafarov/dineit/oauth2/OAuth2ResourceServer.java
@@ -1,9 +1,12 @@
 package emil.dzhafarov.dineit.oauth2;
 ;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 
 import static emil.dzhafarov.dineit.oauth2.AuthorizationServerConfig.tokenStore;
 
@@ -13,5 +16,16 @@ public class OAuth2ResourceServer extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
         resources.tokenStore(tokenStore).resourceId("oauth2-resource");
+    }
+
+    @Primary
+    @Bean
+    public RemoteTokenServices tokenService() {
+        RemoteTokenServices tokenService = new RemoteTokenServices();
+        tokenService.setCheckTokenEndpointUrl(
+                "https://dineit.herokuapp.com/oauth/check_token");
+        tokenService.setClientId("dine-it-client");
+        tokenService.setClientSecret("dine-it-client-pass");
+        return tokenService;
     }
 }
