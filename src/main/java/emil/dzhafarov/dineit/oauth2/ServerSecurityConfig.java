ff908e13f8ed4f4554e5c02094d7ee8ff4aa8686
DzhafarovEmil <johndoe@example.com>
2017-12-14 17:57:41 +0200

    fvbfbf

--- src/main/java/emil/dzhafarov/dineit/oauth2/ServerSecurityConfig.java
+++ src/main/java/emil/dzhafarov/dineit/oauth2/ServerSecurityConfig.java
@@ -1,27 +1,28 @@
 package emil.dzhafarov.dineit.oauth2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @Configuration
-@EnableResourceServer
 public class ServerSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("john").password("123").roles("USER");
+        auth.userDetailsService(userDetailsService);
     }
 
     @Override
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManager();
+        return super.authenticationManagerBean();
     }
 
     @Override
@@ -32,5 +33,4 @@ public class ServerSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/register-customer/").permitAll()
                 .antMatchers("/login").permitAll();
     }
-
 }
