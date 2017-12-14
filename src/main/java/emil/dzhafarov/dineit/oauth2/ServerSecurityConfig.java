837474598df0765389750019866fd5419879a940
DzhafarovEmil <johndoe@example.com>
2017-12-14 18:01:04 +0200

    fvbfbf

--- src/main/java/emil/dzhafarov/dineit/oauth2/ServerSecurityConfig.java
+++ src/main/java/emil/dzhafarov/dineit/oauth2/ServerSecurityConfig.java
@@ -3,6 +3,7 @@ package emil.dzhafarov.dineit.oauth2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -26,6 +27,7 @@ public class ServerSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    @Lazy
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/api/**").authenticated()
