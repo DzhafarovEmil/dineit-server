8832c9eb3639ba53dc51da693bf18ef15405c58f
DzhafarovEmil <johndoe@example.com>
2017-12-14 15:32:31 +0200

    fvbfbf

--- src/main/java/emil/dzhafarov/dineit/oauth2/AuthorizationServerConfig.java
+++ src/main/java/emil/dzhafarov/dineit/oauth2/AuthorizationServerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpMethod;
@@ -28,11 +29,9 @@ import javax.sql.DataSource;
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
     @Autowired
+    @Lazy
     @Qualifier("authenticationManagerBean")
     private AuthenticationManager authenticationManager;
-
-    @Autowired
-    private Environment env;
 
     @Value("classpath:schema.sql")
     private Resource schemaScript;
--- src/main/java/emil/dzhafarov/dineit/oauth2/ServerSecurityConfig.java
+++ src/main/java/emil/dzhafarov/dineit/oauth2/ServerSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -23,6 +24,7 @@ public class ServerSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     @Bean
+    @Lazy
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
