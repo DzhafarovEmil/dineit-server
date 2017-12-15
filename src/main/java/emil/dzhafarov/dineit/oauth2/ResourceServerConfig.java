f50081a6d8429bd3840a2ca8dd5ae405d764a58c
DzhafarovEmil <johndoe@example.com>
2017-12-15 20:42:14 +0200

    vfdnvndj

--- src/main/java/emil/dzhafarov/dineit/oauth2/ResourceServerConfig.java
+++ src/main/java/emil/dzhafarov/dineit/oauth2/ResourceServerConfig.java
@@ -32,8 +32,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.
-                csrf().disable()
+        http
                 .authorizeRequests()
                 .antMatchers("/api/**").authenticated()
                 .antMatchers(
