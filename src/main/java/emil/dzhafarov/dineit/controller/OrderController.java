ce7cf17a4120955e031b9c8356a107730750f28c
DzhafarovEmil <johndoe@example.com>
2017-12-17 07:57:58 +0200

    ffnvn

--- src/main/java/emil/dzhafarov/dineit/controller/OrderController.java
+++ src/main/java/emil/dzhafarov/dineit/controller/OrderController.java
@@ -104,7 +104,7 @@ public class OrderController {
             byte[] bytes = getQRCodeImage(order.toString());
             System.out.println("ORDER ==> " + order.toString());
             System.out.println("ENCODED VALUES ===> " + Arrays.toString(encodeToBase64(order.toString().getBytes()).getBytes()));
-            System.out.println("NEW VALUES ==> " + Arrays.toString(bytes));
+            System.out.println("NEW VALUES ==> " + decodeFromBase64(new String(bytes)));
             QRCode objCode = new QRCode(new String(bytes, Charset.forName("UTF-8")).replaceAll("\u0000",""));
             objCode.setId(qrCodeService.create(objCode));
             order.setQrCode(objCode);
